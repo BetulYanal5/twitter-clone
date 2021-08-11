3335d4a1aee8b8eb7ea71d5d3f4a8e6ce7ccda0c
betulyanal1@gmail.com <betulyanal1@gmail.com>
2021-08-11 18:13:15 +0300

    second commit

---
+++ src/layout/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className="w-64 bg-blue-200">
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/layout/Widgets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Widgets = () => {
+    return (
+        <aside className="bg-green-200 w-72">
+            <h1>TimeLine</h1>
+        </aside>
+    )
+}
+
+export default Widgets
