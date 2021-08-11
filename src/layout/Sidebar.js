38da2bd5d9db1308c062f5e6d65add30655fee17
betulyanal1@gmail.com <betulyanal1@gmail.com>
2021-08-11 19:09:27 +0300

    third commit

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import {
+    BookmarksIcon,
+    ExploreIcon,
+    HomeIcon,
+    ListsIcon,
+    MessagesIcon,
+    MoreIcon,
+    NotificationsIcon,
+    ProfileIcon,
+  } from "../icons/Icon";
 const Sidebar = () => {
     return (
         <div className="w-64 bg-blue-200">
             <h1>Sidebar</h1>
+            <HomeIcon/>
+            <ExploreIcon/>
+            <NotificationsIcon/>
+            <MessagesIcon/>
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
         </div>
     )
 }
