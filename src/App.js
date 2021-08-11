8d62e4139afbbcb48453676242fb377274a4532f
betulyanal1@gmail.com <betulyanal1@gmail.com>
2021-08-11 17:48:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="min-h-screen max-w-5xl mx-auto bg-primary-base">App</h1>
+      
+    </div>
+  )
+}
+
+export default App
